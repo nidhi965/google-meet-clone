23bc9a02729c8e8860620e5a9b12c7af08cc12cb
nidhi965 <kumarinidhi98821@gmail.com>
2024-02-23 11:36:58 +0530

    final

---
+++ hooks/usePeer.js
@@ -0,0 +1,33 @@
+import { useEffect,useState,useRef } from "react";
+import { useSocket } from "@/context/socket";
+import { useRouter } from "next/router";
+ const usePeer =()=>{
+    const socket=useSocket()
+    const roomId=useRouter().query.roomId
+    const [peer, setPeer] = useState(null)
+    const [myId, setMyId] = useState('')
+    const isPeerSet = useRef(false)
+
+    useEffect(()=>{
+        if (isPeerSet.current || !roomId || !socket ) return;
+        isPeerSet.current = true;
+        (async function initPeer() {
+         const   myPeer = new (await import('peerjs')).default()
+            setPeer(myPeer)
+
+         
+            myPeer.on('open', (id) => {
+                console.log(`your peer id is ${id}`)
+                setMyId(id)
+                socket?.emit('join-room',roomId,id)
+              
+            })
+        })()
+    },[roomId,socket])
+
+    return {
+        peer,
+        myId
+    }
+}
+export default usePeer;
--- pages/api/socket.js
+++ pages/api/socket.js
@@ -9,6 +9,22 @@ const SocketHandler = (req,res) => {
         // Whenever any client(browser) would connect this part will run
         io.on('connection',(socket)=>{
             console.log('server is connected')
+
+            socket.on('join-room', (roomId, userId) => {
+                console.log(`a new user ${userId} joined room ${roomId}`)
+                socket.join(roomId)
+                socket.broadcast.to(roomId).emit('user-connected', userId)
+            })
+
+            socket.on('user-toggle-audio', (userId, roomId) => {
+                socket.join(roomId)
+                socket.broadcast.to(roomId).emit('user-toggle-audio', userId)
+            })
+
+            socket.on('user-toggle-video', (userId, roomId) => {
+                socket.join(roomId)
+                socket.broadcast.to(roomId).emit('user-toggle-video', userId)
+            })
         }) 
     }
 res.end();
